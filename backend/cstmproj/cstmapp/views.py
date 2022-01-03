519fe93099b912f3c73bf6df10c5dc9abeeb9bdd
nematollahi.1999 <nematollahi.1999@gmail.com>
2022-01-03 14:18:54 +0330

    User serializer created and passed to the app views.

--- backend/cstmproj/cstmapp/views.py
+++ backend/cstmproj/cstmapp/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.models import User
+from cstmapp.serializers import UserSerializer
+from rest_framework import viewsets
 
 # Create your views here.
 def index(request):
     return HttpResponse("<h1>Hello Bitch!</h1>")
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serialize_class = UserSerializer
