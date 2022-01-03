573b617dd8e3111df209f01a228d3de88376e4f7
nematollahi.1999 <nematollahi.1999@gmail.com>
2022-01-03 14:01:54 +0330

    serializers.py created.

--- backend/cstmproj/cstmapp/serializers.py
+++ backend/cstmproj/cstmapp/serializers.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import User
 from django.db.models import fields
-from django.contrib.auth.models import Model
+from django.contrib.auth.models import User
 from rest_framework import serializers
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
