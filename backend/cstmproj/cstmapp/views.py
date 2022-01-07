b4a425732fb5b5c689635d235431462fe73e6ec9
nematollahi.1999 <nematollahi.1999@gmail.com>
2022-01-08 00:34:40 +0330

    GET,POST,DELETE APIs created in views.py.

--- backend/cstmproj/cstmapp/views.py
+++ backend/cstmproj/cstmapp/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.models import User
-from cstmapp.serializers import UserSerializer
-from rest_framework import viewsets
+from cstmapp.models import Plist
+from cstmapp.serializers import UserSerializer, PlistSerializer
+from rest_framework import viewsets, status
+from rest_framework.parsers import JSONParser
+from django.http.response import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
 def index(request):
@@ -11,3 +15,28 @@ def index(request):
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+#APIs
+# /cstmhome/
+
+@csrf_exempt
+def product_list(request):
+    #Get all
+    if request.method == 'GET':
+        products = Plist.objects.all()
+        products_serializer = PlistSerializer(products, many=True)
+        return JsonResponse(products_serializer.data, safe=False)
+
+    #Add one
+    if request.method == 'POST':
+        product_data = JSONParser().parser(request)
+        product_serializer = PlistSerializer(data=product_data)
+        if product_serializer.is_valid():
+            product_serializer.save()
+            return JsonResponse(product_serializer.data, status=status.HTTP_201_CREATED)
+        return JsonResponse(product_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    #Delete
+    if request.method == 'DELETE':
+        Plist.objects.all().delete()
+        return HttpResponse(status=status.HTTP_204_NO_CONTENT)
