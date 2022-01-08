cac4ce1fa7051bd9c8928b22b6915efdc0679e44
nematollahi.1999 <nematollahi.1999@gmail.com>
2022-01-08 11:54:54 +0330

    All Views GET, PUT, ... added.

--- backend/cstmproj/cstmapp/views.py
+++ backend/cstmproj/cstmapp/views.py
@@ -36,7 +36,33 @@ def product_list(request):
             return JsonResponse(product_serializer.data, status=status.HTTP_201_CREATED)
         return JsonResponse(product_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    #Delete
+    #Delete all
     if request.method == 'DELETE':
         Plist.objects.all().delete()
+        return HttpResponse(status=status.HTTP_204_NO_CONTENT)
+
+@csrf_exempt
+def product_detail(request, primary_key):
+    try:
+        product = Plist.objects.get(pk=primary_key)
+    except Plist.DoesNotExist:
+        return HttpResponse(status=status.HTTP_404_NOT_FOUND)
+
+    #Retreive one
+    if request.method == 'GET':
+        product_serializer = PlistSerializer(product)
+        return JsonResponse(product_serializer.data)
+
+    #Update one record
+    if request.method == 'PUT':
+        product_data = JSONParser().parser(request)
+        product_serializer = PlistSerializer(product, data=product_data)
+        if product_serializer.is_valid():
+            product_serializer.save()
+            return JsonResponse(product_serializer.data)
+        return JsonResponse(product_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    #Delete one record
+    if request.method == 'DELETE':
+        product.delete()
         return HttpResponse(status=status.HTTP_204_NO_CONTENT)
