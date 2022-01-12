03d51cabd78d03fe37d9cb4d6040a2c95173ca4f
nematollahi.1999 <nematollahi.1999@gmail.com>
2022-01-12 16:35:21 +0330

    Backend stuff finished temporarily.

--- backend/cstmproj/cstmproj/settings.py
+++ backend/cstmproj/cstmproj/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'cstmapp.apps.CstmappConfig',
     'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -49,6 +50,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.CorsMiddleware',
+]
+
+CORS_ORIGIN_WHITELIST = [
+    'http://localhost:4200',
 ]
 
 ROOT_URLCONF = 'cstmproj.urls'
